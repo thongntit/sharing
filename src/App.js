5f1ef99e566dee027c856220fa7429c52a4ffea4
Thong Nguyen <thong.nt@teko.vn>
2020-05-18 17:47:59 +0700

    temp commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { news } from "./data";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.refObj = {};
+  }
+  handleClick = (noEle) => {
+    this.refObj[noEle].scrollIntoView();
+  };
+  render() {
+    const { content } = news;
+    return (
+      <div className="Container">
+        <div className="title">
+          <strong>{news.title}</strong>
+        </div>
 
-function App() {
+        <div className="table-of-content">
+          <strong>Table of content</strong>
+          {content &&
+            content.map((para, i) => {
+              return (
+                <ClickableHeading
+                  handleClick={this.handleClick}
+                  heading={para.heading}
+                  index={i}
+                />
+              );
+            })}
+        </div>
+
+        {content &&
+          content.map((para, i) => {
+            return (
+              <Parapraph
+                heading={para.heading}
+                detail={para.detail}
+                refObj={this.refObj}
+                index={i}
+              />
+            );
+          })}
+      </div>
+    );
+  }
+}
+const ClickableHeading = (props) => {
+  const onClick = () => {
+    props.handleClick(props.index);
+  };
+  return <div onClick={onClick}>{props.heading}</div>;
+};
+const Parapraph = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="paragraph"
+      ref={(ref) => {
+        props.refObj[props.index] = ref;
+      }}
+    >
+      <strong>{props.heading}</strong>
+      <br />
+      {props.detail}
     </div>
   );
-}
-
+};
 export default App;
